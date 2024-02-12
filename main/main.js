8603053b4590bf8043ae57eb6d1e36854534aa88
diana <dskripka005@gmail.com>
2024-02-12 15:16:21 +0200

    fix

--- main/main.js
+++ main/main.js
@@ -1,34 +1,58 @@
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            const dataSrc = img.getAttribute('data-src');
-            // Змінює src на data-src
-            img.setAttribute('src', dataSrc);
-           // прибераю observer
-            observer.unobserve(img);
-        }
-    });
-}
-
-const observer = new IntersectionObserver(handleIntersection, {
-    root: null, 
-    rootMargin: '0px', 
-    threshold: 0.5
-});
 
 const images = document.querySelectorAll('img[data-src]');
-
-images.forEach(img => {
-    observer.observe(img);
+const options = {
+  rootMargin: '0px',
+};
+function Intersectionhandle(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src; 
+      observer.unobserve(img); 
+    }
+  });
+}
+const observer = new IntersectionObserver(Intersectionhandle, options);
+images.forEach(image => {
+  observer.observe(image);
 });
 
-const loadImgBtn = document.getElementById('loadImgBtn');
 
-loadImgBtn.addEventListener('click', () => {
-    images.forEach(img => {
-        observer.observe(img);
-    });
-// приберає кнопку після натискання
-    loadImgBtn.style.display = 'none';
-});
+
+
+
+
+// function handleIntersection(entries, observer) {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             const img = entry.target;
+//             const dataSrc = img.getAttribute('data-src');
+//             // Змінює src на data-src
+//             img.setAttribute('src', dataSrc);
+//            // прибераю observer
+//             observer.unobserve(img);
+//         }
+//     });
+// }
+
+// const observer = new IntersectionObserver(handleIntersection, {
+//     root: null, 
+//     rootMargin: '0px', 
+//     threshold: 0.5
+// });
+
+// const images = document.querySelectorAll('img[data-src]');
+
+// images.forEach(img => {
+//     observer.observe(img);
+// });
+
+// const loadImgBtn = document.getElementById('loadImgBtn');
+
+// loadImgBtn.addEventListener('click', () => {
+//     images.forEach(img => {
+//         observer.observe(img);
+//     });
+// // приберає кнопку після натискання
+//     loadImgBtn.style.display = 'none';
+// });
